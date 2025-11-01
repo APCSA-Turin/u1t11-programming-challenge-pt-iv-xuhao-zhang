88db2a2a68774d567f1ceeeff22c02f918b4d631
Xuhao Zhang <xuhaoz@nycstudents.net>
2025-11-01 03:07:49 +0000

    1

--- src/main/java/com/example/project/StringProblems.java
+++ src/main/java/com/example/project/StringProblems.java
@@ -1,24 +1,21 @@
 package com.example.project;
-
-public class StringProblems {
-    // empty constructor
-    public StringProblems() {
-    }
-
+public class StringProblems{
+    //empty constructor
+    public StringProblems(){}
     // Given a string, return true if it ends in "ly".
     // endsLy("oddly") → true
     // endsLy("y") → false
     // endsLy("oddy") → false
-    public boolean endsLy(String x) {
-        try {
-            if (x.substring(x.length() - 2).equals("ly")) {
+    public boolean endsLy(String x){
+            if (x.endsWith("ly")) {
                 return true;
+            } else {
+                return false;
             }
-        } catch (StringIndexOutOfBoundsException e) {
-            return false;
-        }
-        return false;
     }
+
+
+
 
     // Given two strings, append them together (known as "concatenation")
     // and return the result. However, if the concatenation creates a double-char,
@@ -26,51 +23,50 @@ public class StringProblems {
     // conCat("abc", "cat") → "abcat"
     // conCat("dog", "cat") → "dogcat"
     // conCat("abc", "") → "abc"
-    public String conCat(String s1, String s2) {
-        try {
-            if (s1.charAt(s1.length() - 1) == (s2.charAt(0))) {
-            return s1 + s2.substring(1);
+    public String conCat(String s1, String s2){
+        String lastCharacter1 = (s1.substring(s1.length() - 1));
+        if (s2.equals("")) {
+            return s1;
         }
-        return s1 + s2;
-    } catch (StringIndexOutOfBoundsException e) {
-        return s1 + s2;
+        String firstCharacter2 = (s2.substring(0, 1));
+
+
+        if (lastCharacter1.equals(firstCharacter2)) {
+            return s1 + s2.substring(1);
+        } else {
+            return s1 + s2;
+        }
     }
-}
+
 
     // Given a string, return a version without the first 2 chars.
-    // Except keep the first char if it is 'a' and keep the second char if it is
-    // 'b'.
+    // Except keep the first char if it is 'a' and keep the second char if it is 'b'.
     // The string may be any length. Harder than it looks.
     // deFront("Hello") → "llo"
     // deFront("java") → "va"
     // deFront("away") → "aay"
-    // deFront("aapple") ->"apple"
-    // deFront("abee") - > "abee"
-    // deFront("ab") -> "ab"
-    // deFront("a") -> "a"
-    // deFront("ba") -> ""
-    public String deFront(String s1) {
-        try {
-            if ((s1.charAt(0) == "a".charAt(0)) && (s1.charAt(1) == "b".charAt(0))) {
-                return s1;
-            } else if (s1.charAt(0) == "a".charAt(0)) {
-                return "a" + s1.substring(2);
-            } else if (s1.charAt(1) == "b".charAt(0)) {
-                return s1.substring(1);
-            } else {
-                return s1.substring(2);
-            }
-        } catch (StringIndexOutOfBoundsException e) {
-            if (s1.length() == 0) {
-                return "";
-            }
-            if (s1.charAt(0) == "a".charAt(0)) {
-                return s1;
-            }
-            return "";
+    //deFront("aapple") ->"apple"
+    //deFront("abee") - > "abee"
+    //deFront("ab") -> "ab"
+    //deFront("a") -> "a"
+    //deFront("ba") -> ""
+    public String deFront(String s1){
+        String origReturn = s1.substring(2);
+        String firstLetter = s1.substring(0, 1);
+        String secondLetter = s1.substring(1,2);
+
+
+        if (secondLetter.equals("b")) {
+            origReturn = secondLetter + origReturn;
         }
+        if (firstLetter.equals("a")) {
+            origReturn = firstLetter + origReturn;
+        }
+        return origReturn;
     }
 
+
+   
     // Given a string, if the first or last chars are 'x',
     // return the string without those 'x' chars, and otherwise
     // return the string unchanged.
@@ -78,51 +74,72 @@ public class StringProblems {
     // withoutX("xHi") → "Hi"
     // withoutX("Hxix") → "Hxi"
     // withoutX("") → ""
-    public String withoutX(String s1) {
-        if (s1.charAt(0) == "x".charAt(0)) {
-            if (s1.charAt(s1.length() - 1) == "x".charAt(0)) {
-                try {
-                    return (s1.substring(1, s1.length() - 1));
-                } catch (StringIndexOutOfBoundsException e) {
-                    return ("");
-                }
-            } else {
-                return s1.substring(1);
-            }
+    public String withoutX(String s1){
+        String origReturn = s1;
+        String firstLetter = s1.substring(0, 1);
+        String lastLetter = s1.substring(s1.length() - 1, s1.length());
+        if (firstLetter.equals("x") && lastLetter.equals("x")) {
+            origReturn = origReturn.substring(1, s1.length() - 1);
+            return origReturn;
         }
-        if (s1.charAt(s1.length() - 1) == "x".charAt(0)) {
-            return s1.substring(0, s1.length() - 1);
+        if (s1.equals("")) {
+            return origReturn;
         }
-        return s1;
+        if (lastLetter.equals("x")) {
+            origReturn = origReturn.substring(0, s1.length() - 1);
+        }
+        if (firstLetter.equals("x")) {
+            origReturn = origReturn.substring(1, s1.length());
+        }
+        return origReturn;
     }
 
+
     // Given a string str, if the string starts with "f" return "Fizz".
-    // If the string ends with "b" return "Buzz". If both the "f" and "b"
-    // conditions are true, return "FizzBuzz". In all other cases, return the
-    // string unchanged. (See also: FizzBuzz Code)
+    //  If the string ends with "b" return "Buzz". If both the "f" and "b"
+    //  conditions are true, return "FizzBuzz". In all other cases, return the
+    //  string unchanged. (See also: FizzBuzz Code)
     // fizzString("fig") → "Fizz"
     // fizzString("dib") → "Buzz"
     // fizzString("fib") → "FizzBuzz"
-    public String fizzString(String s1) {
-        try {
-            if (s1.charAt(0) == "f".charAt(0) && s1.charAt(s1.length() - 1) == "b".charAt(0)) {
-                return "FizzBuzz";
-            }
-            if (s1.charAt(0) == "f".charAt(0)) {
-                return "Fizz";
-            }
-            if (s1.charAt(s1.length() - 1) == "b".charAt(0)) {
-                return "Buzz";
-            }
-            return s1;
-        } catch (StringIndexOutOfBoundsException e) {
-            if (s1.equals("f")) {
-                return "Fizz";
-            }
-            if (s1.equals("b")) {
-                return "Buzz";
-            }
-            return s1;
+    public String fizzString(String s1){
+        String origReturn = s1;
+        String firstLetter = s1.substring(0, 1);
+        String lastLetter = s1.substring(s1.length() - 1);
+        if (lastLetter.equals("b") && firstLetter.equals("f")) {
+            origReturn = "FizzBuzz";
+            return origReturn;
         }
+        if (lastLetter.equals("b")) {
+            origReturn = "Buzz";
+        }
+        if (firstLetter.equals("f")) {
+            origReturn = "Fizz";
+        }
+        return origReturn;
+    }
+
+
+    // Given an int n, return the string form of the number followed
+    // by "!". So the int 6 yields "6!". Except if the number is
+    // divisible by 3 use "Fizz" instead of the number, and if the
+    // number is divisible by 5 use "Buzz", and if divisible by both 3 and 5, use "FizzBuzz".
+    //  Note: the % "mod" operator computes the remainder after division, so 23 % 10 yields 3.
+    //   What will the remainder be when one number divides evenly into another?
+    // fizzString2(1) → "1!"
+    // fizzString2(2) → "2!"
+    // fizzString2(3) → "Fizz!"
+    public String fizzString2(int x){
+        if (x % 5 == 0 && x % 3 == 0) {
+            return "FizzBuzz!";
+        }
+        if (x % 3 == 0) {
+            return "Fizz!";
+        }
+        if (x % 5 == 0) {
+            return "Buzz!";
+        }
+        return x + "!";
     }
 }
+
